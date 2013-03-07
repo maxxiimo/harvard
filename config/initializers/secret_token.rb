55c58bbfff124f22f8344edb8e4262c0e57ca0af
Chris Maxwell <cmaxwell@ojala.com>
2013-03-07 14:53:01 -0500

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Harvard::Application.config.secret_token = '23bdb9b3ec29e2062d63f2fb779847bc6305af43b91d045d9c2b0679118bae2e639e7ddee4fd817f42fe52513493138b9519ca5ff1b4e5d2bd28ef25784b0e2f'
