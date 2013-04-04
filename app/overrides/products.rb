d59ca190bbb8d21631e11940e04c0bab17125edf
Chris Maxwell <cmaxwell@ojala.com>
2013-04-04 14:09:56 -0400

    Change product store image sizes.

---
+++ app/overrides/products.rb
@@ -0,0 +1,6 @@
+# FIXME ccm: DELETE, probably will not be used.
+
+# Deface::Override.new(:virtual_path => "spree/shared/_products",
+                     # :replace_contents => "",
+                     # :text => "",
+                     # :name => "products")
